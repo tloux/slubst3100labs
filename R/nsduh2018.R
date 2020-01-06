a3c87a03625991488ffe789e18c32de681682bd6
tloux <travis.loux@gmail.com>
2020-01-06 10:17:47 -0600

    0.0.1
    R package with 2018 NSDUH data and documentation

---
+++ R/nsduh2018.R
@@ -0,0 +1,11 @@
+#' 2018 National Survey on Drug Use and Health (NSDUH)
+#' 
+#' The National Survey on Drug Use and Health (NSDUH) series, formerly titled National Household Survey on Drug Abuse, is a major source of statistical information on the use of illicit drugs, alcohol, and tobacco and on mental health issues among members of the U.S. civilian, non-institutional population aged 12 or older. The survey tracks trends in specific substance use and mental illness measures and assesses the consequences of these conditions by examining mental and/or substance use disorders and treatment for these disorders.
+#' 
+#' For more information, see the NSDUH 2018 webapage: \url{https://www.datafiles.samhsa.gov/study-dataset/national-survey-drug-use-and-health-2018-nsduh-2018-ds0001-nid18758}. In particular, the survey codebook will be important.
+#' 
+#' @format A data frame with 56313 rows and 2691 columns
+#' 
+#' @seealso \code{nsduh2018sm}
+#' 
+"nsduh2018"
---
+++ R/nsduh2018sm.R
@@ -0,0 +1,11 @@
+#' Subset of 2018 National Survey on Drug Use and Health (NSDUH)
+#' 
+#' A random sample of 1000 rows from the 2018 NSDUH survey
+#' 
+#' For more information, see the NSDUH 2018 webapage: \url{https://www.datafiles.samhsa.gov/study-dataset/national-survey-drug-use-and-health-2018-nsduh-2018-ds0001-nid18758}. In particular, the survey codebook will be important.
+#' 
+#' @format A data frame with 1000 rows and 2691 columns
+#' 
+#' @seealso \code{nsduh2018}
+#' 
+"nsduh2018sm"
